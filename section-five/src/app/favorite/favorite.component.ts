505928d8dba9f0eb50e2f3f411d313d42a16530c
Norm Glomski <nglomski@gmail.com>
2017-10-15 17:52:57 -0500

    Completed Input Props, Output Props, and Passing Events.

---
+++ section-five/src/app/favorite/favorite.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+  @Input('isFavorite') isSelected: boolean;
+  @Output() change = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    this.isSelected = !this.isSelected;
+    this.change.emit({ isFavorite: this.isSelected });
+  }
+
+}
+
+export interface FavoriteChangedEventArgs {
+  isFavorite: boolean
+}
