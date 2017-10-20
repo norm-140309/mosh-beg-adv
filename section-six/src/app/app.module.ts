4ac2d625799d70d60ce30ac1d13b2218ec47c276
Norm Glomski <nglomski@gmail.com>
2017-10-20 15:30:58 -0500

    Completed ngif, hidden and ngSwitchCase.

--- section-six/src/app/app.module.ts
+++ section-six/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FavoriteComponent,
-    PanelComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
