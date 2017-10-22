5594b7417248e24cac531deee699adadd1a7c42b
Norm Glomski <nglomski@gmail.com>
2017-10-22 15:51:55 -0500

    Completed section-six and created the section-seven project.

--- section-six/src/app/app.component.ts
+++ section-six/src/app/app.component.ts
@@ -6,6 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [1, 2];
   viewMode = 'map';
+  isSelected = true;
+  canSave = true;
+  courses = [
+    { id: 1, name: 'course1 '},
+    { id: 2, name: 'course2 '},
+    { id: 3, name: 'course3 '}
+  ];
+  task = {
+    title: 'Review applications',
+    assignee: {
+      name: 'John Smith'
+    }
+  };
+  onAdd() {
+    this.courses.push({ id:4, name: 'course 4 ' });
+  }
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  onUpdate(course) {
+    course.name = 'UPDATED';
+  }
+  loadCourses() {
+    this.courses = [
+      { id: 11, name: 'course11 '},
+      { id: 12, name: 'course12 '},
+      { id: 13, name: 'course13 '}
+    ];
+  }
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
+  onClick() {
+    this.isSelected = !this.isSelected;
+  }
 }
