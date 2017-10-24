2feef3cceba737a456855193767818e0942adfcf
Norm Glomski <nglomski@gmail.com>
2017-10-24 12:39:46 -0500

    Finished adding an asynchronous validator rule to the username field.

--- section-eight/src/app/signup-form/username.validators.ts
+++ section-eight/src/app/signup-form/username.validators.ts
@@ -6,4 +6,16 @@ export class UsernameValidators {
             return { cannotContainSpace: true };
         }
     }
+
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+        return new Promise((resolve, reject) => {
+            setTimeout( () => {
+                if (control.value === 'norm') {
+                    resolve({ shouldBeUnique: true });
+                } else {
+                    resolve(null);
+                }
+            }, 2000);
+        });
+    }
 }
